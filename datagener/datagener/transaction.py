962d98883e2ad69752d4bd2f92a8997334bee9c1
nontapat <nonimm75@hotmail.com>
2020-09-24 23:34:51 +0700

    move names into name_dict dir

--- datagener/datagener/transaction.py
+++ datagener/datagener/transaction.py
@@ -3,12 +3,12 @@ import random
 import string
 
 def get_names(n_rec):
-    lineList = [line.rstrip('\n') for line in open('/home/nontapat/Documents/my packages/datagener/names.txt')]
+    lineList = [line.rstrip('\n') for line in open('/home/nontapat/Documents/my packages/datagener/name_dict/names.txt')]
     name_list = random.choices(lineList, k=n_rec)
     return name_list
 
 def get_emails(name_list):
-    company_name_list = random.choices([line.rstrip('\n') for line in open('/home/nontapat/Documents/my packages/datagener/company-names.txt')], k=len(name_list))
+    company_name_list = random.choices([line.rstrip('\n') for line in open('/home/nontapat/Documents/my packages/datagener/name_dict/company-names.txt')], k=len(name_list))
     surname_list = random.choices(string.ascii_letters, k=len(name_list))
     email_list= list(map(lambda x, y, z: x+ '.' +y + '@mail.' + z + '.co', name_list, surname_list, company_name_list))
     return email_list
