bf019f4c39858f49911e26459cce7cb463bd65c8
nontapat <nonimm75@hotmail.com>
2020-09-24 23:29:35 +0700

    Initialize package datagener
    create a venv
    add pandas
    activate the venv :-P

---
+++ datagener/datagener/transaction.py
@@ -0,0 +1,17 @@
+# source /home/nontapat/.cache/pypoetry/virtualenvs/datagener-bx0uWqDJ-py3.8/bin/activate
+import random
+import string
+
+def get_names(n_rec):
+    lineList = [line.rstrip('\n') for line in open('/home/nontapat/Documents/my packages/datagener/names.txt')]
+    name_list = random.choices(lineList, k=n_rec)
+    return name_list
+
+def get_emails(name_list):
+    company_name_list = random.choices([line.rstrip('\n') for line in open('/home/nontapat/Documents/my packages/datagener/company-names.txt')], k=len(name_list))
+    surname_list = random.choices(string.ascii_letters, k=len(name_list))
+    email_list= list(map(lambda x, y, z: x+ '.' +y + '@mail.' + z + '.co', name_list, surname_list, company_name_list))
+    return email_list
+
+if __name__=='__main__':
+    print(get_names(10))
